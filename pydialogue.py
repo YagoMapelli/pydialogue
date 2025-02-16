833b4e195f238e47f9a91651e0ee004a165c0419
YagoMapelli <yagomapelli2@gmail.com>
2025-02-16 19:07:14 -0300

    pydialogue.v2.3.4

--- pydialogue.py
+++ pydialogue.py
@@ -1,4 +1,4 @@
-class CustomDialogue:
+class Dialogue:
     def __init__(self, initial_text="", final_text="", error_text="option not found", input_text=">>>", is_loop=True, start_option_markers=" [", end_option_markers="]"):
         self.initial_text = initial_text
         self.final_text = final_text
@@ -7,14 +7,15 @@ class CustomDialogue:
         self.is_loop = is_loop
         self.start_option_markers = start_option_markers
         self.end_option_markers = end_option_markers
-        self.options = {} 
+        self.options = {}
 
-    def add_option(self, name, function, custom_option_marker=False, start_custom_option_marker="", end_custom_option_marker=""):
-        if custom_option_marker:
-            self.options.setdefault(name, [function, start_custom_option_marker, end_custom_option_marker])
-        else:
-            self.options.setdefault(name, [function, self.start_option_markers, self.end_option_markers])
-        #name: [0 - function, 1 - start_custom_option_marker, 2 - end_custom_option_marker]
+    def add_option(self, name, function, start_option_marker=None, end_option_marker=None):
+            self.options[name] = [
+                function,
+                self.start_option_markers if start_option_marker is None else start_option_marker,
+                self.end_option_markers if end_option_marker is None else end_option_marker
+                ]
+        #name: [0 - function, 1 - start_option_marker, 2 - end_option_marker]
 
     def ask_dialogue(self):
         print(self.initial_text)
@@ -34,27 +35,24 @@ class CustomDialogue:
                 print(self.error_text)
             if not self.is_loop: break
 
-class CustomListDialogue:
-    def __init__(self, initial_text="", final_text="", error_text="option not found", input_text=">>>", is_loop=True, list_type=1):
+class ListDialogue:
+    def __init__(self, initial_text="", final_text="", error_text="option not found", input_text=">>>", is_loop=True):
         self.initial_text = initial_text
         self.final_text = final_text
         self.error_text = error_text
         self.input_text = input_text
         self.is_loop = is_loop
-        self.list_type = list_type
         self.options = {}
-        self.enumerated_options = []
 
     def add_option(self, name, function):
-        self.options.setdefault(name, function)
-        self.enumerated_options.append(name)
-        #name: function
+        self.options[len(self.options) + 1] = [name, function]
+        #number: [name, function]
 
     def ask_dialogue(self):
         print(self.initial_text)
 
-        for i, name in enumerate(self.enumerated_options):
-            print(f"{i + 1} - {name}")
+        for i in range(1, self.options):
+            print(f"{i + 1} - {self.options[i + 1][0]}")
 
         if self.final_text != "":
             print(self.final_text)
@@ -69,14 +67,12 @@ class CustomListDialogue:
 #>>>
         while True:
             user_input = input(self.input_text)
-            try:
-                user_input = int(user_input)
-                is_int_input = True
-            except ValueError:
-                is_int_input = False
 
-            if is_int_input and 0 <= user_input - 1 <= len(self.enumerated_options):
-                self.options[self.enumerated_options[user_input - 1]]()
+            try: user_input = int(user_input)
+            except ValueError: pass
+
+            if isinstance(user_input, int) and user_input in self.options:
+                self.options[user_input][1]()
                 return
             else:
                 print(self.error_text)
